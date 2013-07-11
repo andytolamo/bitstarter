6053e238a0097e8fbb8b7601a2ddfb777d26275c
Ubuntu <ubuntu@ip-10-251-49-4.us-west-2.compute.internal>
2013-07-11 20:53:14 +0000

    added buffer to read

--- web.js
+++ web.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var fs = require('fs');
+var buf = require('buf');
 var app = express.createServer(express.logger());
 
 var content = fs.readFileSync('index.html');
+content = buf.toString(content);
 
 app.get('/', function(request, response) {
   response.send(content);
